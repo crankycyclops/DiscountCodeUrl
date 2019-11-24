f79965b9f31c2179f9153911db7e658e2e0546e6
Jeff Coleman <jeff@jeffcolemanwrites.com>
2019-11-24 10:53:14 -0800

    added success and error messages when user attempts to apply a discount code via URL

--- Plugin/Framework/App/FrontControllerInterface.php
+++ Plugin/Framework/App/FrontControllerInterface.php
@@ -27,6 +27,21 @@ class FrontControllerInterface {
 	 */
 	private $cookieHelper;
 
+	/**
+	 * @var \Magento\SalesRule\Model\Coupon
+	 */
+	private $couponModel;
+
+	/**
+	 * @var \Magento\SalesRule\Model\Rule
+	 */
+	private $ruleModel;
+
+	/**
+	 * @var \Magento\Framework\Message\ManagerInterface
+	 */
+	private $messageManager;
+
 	/************************************************************************/
 
 	/**
@@ -38,11 +53,17 @@ class FrontControllerInterface {
 	public function __construct(
 		\Magento\Framework\App\RequestInterface $request,
 		\Crankycyclops\DiscountCodeUrl\Helper\Config $config,
-		\Crankycyclops\DiscountCodeUrl\Helper\Cookie $cookieHelper
+		\Crankycyclops\DiscountCodeUrl\Helper\Cookie $cookieHelper,
+		\Magento\SalesRule\Model\Coupon $couponModel,
+		\Magento\SalesRule\Model\Rule $ruleModel,
+		\Magento\Framework\Message\ManagerInterface $messageManager
 	) {
 		$this->request = $request;
 		$this->config = $config;
 		$this->cookieHelper = $cookieHelper;
+		$this->couponModel = $couponModel;
+		$this->ruleModel = $ruleModel;
+		$this->messageManager = $messageManager;
 	}
 
 	/************************************************************************/
@@ -61,8 +82,72 @@ class FrontControllerInterface {
 
 			$coupon = $this->request->getParam($this->config->getUrlParameter());
 
+			$invalidMessage = "Discount code <strong>$coupon</strong> is invalid";
+			$expiredMessage = "Unfortunately, the <strong>$coupon</strong> discount code is expired";
+			$consumedMessage = "Unfortunately, the <strong>$coupon</strong> discount code has been fully consumed";
+
 			if ($coupon) {
-				$this->cookieHelper->setCookie($coupon);
+
+				$this->couponModel->loadByCode($coupon);
+
+				if($this->couponModel->getId()) {
+
+					$this->ruleModel->load($this->couponModel->getRuleId());
+
+					if ($this->ruleModel->getId()) {
+
+						$today = strtotime(date("Y-m-d"));
+						$startDay = $this->ruleModel->getFromDate();
+						$expirationDay = $this->ruleModel->getToDate();
+
+						$numUses = $this->couponModel->getTimesUsed();
+						$maxUses = $this->couponModel->getUsageLimit();
+
+						$usesPerCustomer = $this->couponModel->getUsagePerCustomer();
+
+						// Discount code is expired
+						if ($expirationDay && strtotime($expirationDay) < $today) {
+							$this->messageManager->addError(__($expiredMessage));
+						}
+
+						// Discount hasn't started yet
+						else if ($startDay && strtotime($startDay) > $today) {
+							$this->messageManager->addError(__($invalidMessage));
+						}
+
+						// Coupon has already been fully consumed
+						else if ($maxUses && $numUses >= $maxUses) {
+							$this->messageManager->addError(__($consumedMessage));
+						}
+
+						else {
+
+							$successMessage = "Discount code <strong>$coupon</strong> will be applied to your order during checkout";
+
+							if ($usesPerCustomer && $usesPerCustomer > 0) {
+
+								if ($usesPerCustomer > 1) {
+									$successMessage .= " unless you've already fully consumed it (code is only valid for up to $usesPerCustomer orders";
+								} else {
+									$successMessage .= " unless you've already used it (code is only valid for one order";
+								}
+
+								$successMessage .= " per customer)";
+							}
+
+							$this->cookieHelper->setCookie($coupon);
+							$this->messageManager->addSuccess(__($successMessage));
+						}
+					}
+
+					else {
+						$this->messageManager->addError(__($invalidMessage));
+					}
+				}
+
+				else {
+					$this->messageManager->addError(__($invalidMessage));
+				}
 			}
 		}
 	}
